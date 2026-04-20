459136a94fb4baf9ca42ea03ed8ef6319149a3a7
kirankumarkallumari <kiraakkiran143@gmail.com>
2026-04-20 12:06:52 +0530

    added code link button

--- frontend/src/CreatePage.jsx
+++ frontend/src/CreatePage.jsx
@@ -140,6 +140,14 @@ function CreatePage() {
             <div className="mt-4 flex justify-center">
               {/* <QRCodeCanvas value={`http://localhost:5173/view/${code}`} /> */}
               <QRCodeCanvas value={`${window.location.origin}/view/${code}`} />
+              <a
+                href={`${window.location.origin}/view/${code}`}
+                 target="_blank"
+                rel="noopener noreferrer"
+                 className="mt-4 inline-block bg-blue-500 text-white px-4 py-2 rounded-lg hover:scale-105 transition"
+                   >
+                  🔗 Open Link
+                  </a>
             </div>
 
             <button
